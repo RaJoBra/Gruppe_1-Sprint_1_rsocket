930795ae4c82488b39c6c7b3fae56b4d1d29ac52
Raphael <Raphael.Brand@gmx.net>
2021-04-03 17:37:02 +0200

    Set Port in client and server

--- client/src/main/java/com/jbgbh/rSocket/rSocketShellClient.java
+++ client/src/main/java/com/jbgbh/rSocket/rSocketShellClient.java
@@ -1,16 +1,55 @@
 package com.jbgbh.rSocket;
 
+import io.rsocket.SocketAcceptor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
 import org.springframework.shell.standard.ShellComponent;
-import org.springframework.shell.standard.ShellMethod;
 import reactor.core.publisher.Flux;
 
+import javax.annotation.PreDestroy;
 import java.time.Duration;
+import java.util.UUID;
 
 @ShellComponent
 @Slf4j
 public class rSocketShellClient {
+    private final RSocketRequester rsocketRequester;
+
+    @Deprecated
+    @Autowired
+    public rSocketShellClient(RSocketRequester.Builder rsocketRequesterBuilder, RSocketStrategies strategies) {
+        String client = UUID.randomUUID().toString();
+        log.info("Connecting using client ID: {}", client);
+
+        SocketAcceptor responder = RSocketMessageHandler.responder(strategies, new ClientHandler());
+
+        this.rsocketRequester = rsocketRequesterBuilder
+                .setupRoute("shell-client")
+                .setupData(client)
+                .rsocketStrategies(strategies)
+                .rsocketConnector(connector -> connector.acceptor(responder))
+                .connectTcp("localhost", 8080)
+                .block();
+
+        this.rsocketRequester.rsocket()
+                .onClose()
+                .doOnError(error -> log.warn("Connection CLOSED"))
+                .doFinally(consumer -> log.info("Client DISCONNECTED"))
+                .subscribe();
+    }
+
+    public rSocketShellClient(RSocketRequester rsocketRequester) {
+        this.rsocketRequester = rsocketRequester;
+    }
+
+    @PreDestroy
+    void shutdown() {
+        rsocketRequester.rsocket().dispose();
+    }
 
 }
 
